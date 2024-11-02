b88295514b1868b2e12c6a52ab0b7c96a56b3688
RAS <user@MacBook.local>
2024-11-03 00:06:25 +0700

    home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,50 +5,105 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        FocusScope.of(context).unfocus();
-      },
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: Colors.white,
-          appBar: AppBar(
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Scaffold(
             backgroundColor: Colors.white,
-            leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
-            title: const Text(
-              "LARA STORE",
-            ),
-            titleTextStyle: const TextStyle(
-                fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20),
-            centerTitle: true,
-            actions: [
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.shopping_cart_outlined))
-            ],
-          ),
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                Center(
-                  child: Container(
-                    margin: const EdgeInsets.only(top: 30, bottom: 20),
-                    width: MediaQuery.of(context).size.width / 1.2,
-                    child: const TextField(
-                        decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.search),
-                      hintText: "Looking For Shoes",
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black, width: 1),
-                          borderRadius: BorderRadius.all(Radius.circular(30))),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black, width: 1),
-                          borderRadius: BorderRadius.all(Radius.circular(30))),
-                    )),
-                  ),
-                )
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              leading:
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
+              title: const Text(
+                "LARA STORE",
+              ),
+              titleTextStyle: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontSize: 20),
+              centerTitle: true,
+              actions: [
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.shopping_cart_outlined))
               ],
+            ),
+            body: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Center(
+                    child: Container(
+                      margin: const EdgeInsets.only(top: 30, bottom: 20),
+                      width: MediaQuery.of(context).size.width / 1.1,
+                      child: const TextField(
+                          decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.search),
+                        hintText: "Looking For Shoes",
+                        enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 1),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30))),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.black, width: 1),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30))),
+                      )),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          margin: const EdgeInsets.only(left: 10),
+                          width: MediaQuery.of(context).size.width / 3.5,
+                          decoration: BoxDecoration(
+                              color: Colors.blue,
+                              border: Border.all(color: Colors.blue),
+                              borderRadius: BorderRadius.circular(50)),
+                          height: 50,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 6),
+                            child: Row(
+                              children: [
+                                Image.asset(
+                                  "assets/images/nike_logo.png",
+                                  width: MediaQuery.of(context).size.width / 10,
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                const Text(
+                                  "Nike",
+                                  style: TextStyle(color: Colors.white),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 10, top: 10),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Nike Shoes",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
